00f357d325c63fd1fa705da1bd08f4bbc8786abb
PabloMM03 <1V3rsus1@gmail.com>
2023-04-27 17:48:13 +0200

    Layout

--- marketplace_proyectSComents6/resources/views/layouts/app.blade.php
+++ marketplace_proyectSComents6/resources/views/layouts/app.blade.php
@@ -62,21 +62,5 @@
    
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script> 
 
-
-    
-  
-    
-   
-
-{{--Notification --}}
-
-    {{-- <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
-
-    @if (session('message'))
-    <script> 
-    Swal.("{{session('message')}}");
-    </script>
-    @endif --}}
-
 </body>
 </html>
