cd72aca21af2f0cc68333044569cee2032057206
PabloMM03 <1V3rsus1@gmail.com>
2023-05-15 18:52:36 +0200

    Icon

--- marketplace_proyectSComents8/resources/views/layouts/app.blade.php
+++ marketplace_proyectSComents8/resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
 
 <head>
     <meta charset="utf-8">
-<title>Surfside Media</title>
+<title>TradeVibes</title>
 <meta http-equiv="x-ua-compatible" content="ie=edge">
 <meta name="description" content="">
 <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -14,7 +14,7 @@
 <meta property="og:type" content="">
 <meta property="og:url" content="">
 <meta property="og:image" content="">
-<link rel="shortcut icon" type="image/x-icon" href="{{asset('assets/imgs/theme/favicon.ico')}}">
+<link rel="shortcut icon" type="image/x-icon" href="{{asset('assets/imgs/logo/Mi proyecto2.png')}}">
 <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" />
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
 <link rel="stylesheet" href="{{asset('assets/css/main.css')}}">
