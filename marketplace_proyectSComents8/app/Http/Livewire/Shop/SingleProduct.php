df15272e0373c55ed6f4d50fb0cae143c790546e
PabloMM03 <1V3rsus1@gmail.com>
2023-05-25 13:41:21 +0200

    Ratings

--- marketplace_proyectSComents8/app/Http/Livewire/Shop/SingleProduct.php
+++ marketplace_proyectSComents8/app/Http/Livewire/Shop/SingleProduct.php
@@ -74,7 +74,7 @@ class SingleProduct extends Component
                 $product->review_count = $ratings->count();
             }
 
-            // Get the ratings and number of reviews for each product through the product id
+            // Get the ratings and number of reviews for each product through the product realted id
 
             foreach ($this->relacionados as $product) {
                 $ratings = Rating::where('prod_id', $product->id)->get();
